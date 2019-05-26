f36c7ca886752a0a2bafa9d0f5bc557bea8ae8d1
Freddy <46581440+FreddyisFreddy@users.noreply.github.com>
2019-05-25 21:25:00 -0400

    Add files via upload

---
+++ Push_Button/Push_Button.ino
@@ -0,0 +1,22 @@
+
+
+
+void setup() {
+Serial.begin(9600);  
+pinMode(4,INPUT);
+pinMode(13,OUTPUT);
+
+}
+
+void loop() {
+int pushbutton;
+pushbutton = digitalRead(4);
+
+digitalWrite(13,pushbutton);
+Serial.println(pushbutton);
+if (pushbutton == LOW)
+digitalWrite(13,HIGH);
+else digitalWrite(13,LOW);
+}
+
+
